5cb2890842f2643ec6aff961fb809bc097896568
Alumno <Alumno@PC155>
2025-05-23 18:24:33 -0300

    Falta completar algunas validaciones del loginAgregar archivos de proyecto.

---
+++ pryCalvar-IEFI/clsConexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalvar_IEFI
+{
+    internal class clsConexion
+    {
+        private string cadenaConexion = "Server=localhost;Database=BDDCalvar_IEFI;Trusted_Connection=True;";
+
+        // SqlConnection establece la conexion con la bdd
+        public SqlConnection conexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+
+        // "?" por que la funcion puede devolver un numero entero o null. En este caso el id del usuario
+        public int? IniciarSesion(string nombre, string clave)
+        {
+            using (SqlConnection conn = conexion())
+            {
+                string query = "SELECT Id FROM Usuario WHERE Nombre = @nombre AND Contraseña = @clave";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@nombre", nombre);
+                cmd.Parameters.AddWithValue("@clave", clave);
+
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                    return Convert.ToInt32(result);
+                else
+                    return null;
+            }
+        }
+    }
+}
