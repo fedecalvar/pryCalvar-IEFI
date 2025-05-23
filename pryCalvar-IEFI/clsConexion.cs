ba9a8552c875005ce0157614b3ae520246a62fa7
Alumno <Alumno@PC155>
2025-05-23 18:24:33 -0300

    Login 100% funcional.

---
+++ pryCalvar-IEFI/clsConexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryCalvar_IEFI
+{
+    internal class clsConexion
+    {
+        private string cadenaConexion = "Server=localhost;Database=BDDCalvar_IEFI;Trusted_Connection=True;";
+
+        // SqlConnection establece la conexion con la bdd
+        public SqlConnection conexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+
+        // "?" por que la funcion puede devolver un numero entero o null. En este caso el id del usuario
+        public int? IniciarSesion(string usuario, string contrasena)
+        {
+            using (SqlConnection conn = conexion())
+            {
+                string query = "SELECT IdUsuario FROM Usuarios WHERE NombreUsuario = @usuario AND Contrasena = @contrasena";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@usuario", usuario);
+                cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                    return Convert.ToInt32(result);
+                else
+                    return null;
+            }
+        }
+    }
+}
---
+++ pryCalvar-IEFI/frmLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCalvar_IEFI
+{
+    public partial class Form1 : Form
+    {
+
+        clsConexion conexion = new clsConexion();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if(txtUsuario.Text != "" || txtContrasena.Text != "")
+            {
+                string usuario = txtUsuario.Text;
+                string clave = txtContrasena.Text;
+
+                int? usuarioID = conexion.IniciarSesion(usuario, clave);
+
+                if (usuarioID != null)
+                {
+                    frmPrincipal principal = new frmPrincipal(usuarioID.Value);
+                    this.Hide();
+                    principal.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Porfavor ingresa la clave o el usuario.");
+            }
+        }
+    }
+}
